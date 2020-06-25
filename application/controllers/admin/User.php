7d39e4f9c24a0f78e583e0d4caacb58e9d4231c9
unknown <oppet.copet@gmail.com>
2020-06-26 01:55:22 +0700

    oppet

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -6,6 +6,8 @@ class User extends CI_Controller{
     function __construct(){
         parent::__construct();
         $this->load->model('M_user');
+        $this->load->library('form_validation');
+        $this->load->helper('form');
     }
 
     public function index(){
--- application/models/M_user.php
+++ application/models/M_user.php
@@ -4,5 +4,35 @@
             $query=$this->db->get("user");
             return $query->result_array();
         }
+
+        function rules(){
+            return array(
+                array(  'field' => 'nama',
+                        'label' => 'Nama',
+                        'rules' => "required|trim"),
+        
+                array(  'field' => "un",
+                        'label' => "username",
+                        'rules' => "required|trim"),
+        
+                array(  'field' => "pw",
+                        'label' => "password",
+                        'rules' => "required|trim|matches[rpw]|min_length[8]"),
+        
+                array(  'field' => "rpw",
+                        'label' => "re-password",
+                        'rules' => "required|trim|matches[pw]")
+            );
+        }
+
+        public function insert(){
+            $data = [
+                "Nama" => $this->input->post("nama",true),
+                "Username" => $this->input->post("un",true),
+                "Password" => $this->input->post("pw",true),
+            ];
+            $this->db->insert('admin',$data);
+        }
+        
     }
 ?>
