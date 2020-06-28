b2a87b1a0a8286e6ba13bdcd4388ef7ac75aaa13
unknown <hafidzalafaf@gmail.com>
2020-06-28 10:42:58 +0700

    update polaroid pada selamat datang, tapi belum ada fotonye :)

--- application/views/pengguna/templates/slider.php
+++ application/views/pengguna/templates/slider.php
@@ -1,43 +1,144 @@
+  <!DOCTYPE html>
+<html lang="en" class="no-js">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <title>Scattered Polaroids Gallery</title>
+    <meta name="description" content="Scattered Polaroids Gallery: A flat-style take on a Polaroid gallery" />
+    <meta name="keywords" content="scattered polaroids, image gallery, javascript, random rotation, 3d, backface, flat design" />
+    <meta name="author" content="Codrops" />
+    <link rel="stylesheet" type="text/css" href="<?=base_url('') ?>assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url('') ?>assets/css/demo.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url('') ?>assets/css/component.css">
+    <link rel="shortcut icon" href="../favicon.ico">
+    <script src="<?=base_url('') ?>assets/js/modernizr.min.js"></script>
+  </head>
+  <body>
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">Selamat Datang</h1>
-          <h1 data-aos="fade-up">Di</h1>
-          <h1 data-aos="fade-up">Website</h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">Desa Wisata Tanon</h2>
-          <!-- <div data-aos="fade-up" data-aos-delay="800">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
-          </div> -->
+    <section id="hero" class="d-flex align-items-center">
+      <!-- Top Navigation -->
+      <section id="photostack-1" class="photostack photostack-start tanon " style="width: 100%;">
+        <div>
+          <figure>
+            <a href="http://goo.gl/Qw3ND4" class="photostack-img"><img src="<?=base_url('') ?>assets/img/1.jpg" alt="img01"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Serenity Beach</h2>
+            </figcaption>
+          </figure>
+          <figure>
+            <a href="http://goo.gl/fhwlSP" class="photostack-img"><img src="<?=base_url('') ?>assets/img/2.jpg" alt="img02"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Happy Days</h2>
+            </figcaption>
+          </figure>
+          <figure>
+            <a href="http://goo.gl/Jmvr4u" class="photostack-img"><img src="<?=base_url('') ?>assets/img/3.jpg" alt="img03"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Beautywood</h2>
+            </figcaption>
+          </figure>
+          <figure>
+            <a href="http://goo.gl/49lN3k" class="photostack-img"><img src="<?=base_url('') ?>assets/img/4.jpg" alt="img04"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Heaven of time</h2>
+            </figcaption>
+          </figure>
+          <figure>
+            <a href="http://goo.gl/NJ1Dhf" class="photostack-img"><img src="<?=base_url('') ?>assets/img/5.jpg" alt="img05"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Speed Racer</h2>
+            </figcaption>
+          </figure>
+          <figure>
+            <a href="http://goo.gl/Ms7VDl" class="photostack-img"><img src="<?=base_url('') ?>assets/img/6.jpg" alt="img06"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Forever this</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/7.jpg" alt="img07"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Lovely Green</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/8.jpg" alt="img08"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Wonderful</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/9.jpg" alt="img09"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Love Addict</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/10.jpg" alt="img10"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Friendship</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/11.jpg" alt="img11"/></a>
+            <figcaption>
+              <h2 class="photostack-title">White Nights</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/12.jpg" alt="img12"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Serendipity</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/13.jpg" alt="img13"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Pure Soul</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/14.jpg" alt="img14"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Winds of Peace</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/15.jpg" alt="img15"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Shades of blue</h2>
+            </figcaption>
+          </figure>
+          <figure data-dummy>
+            <a href="#" class="photostack-img"><img src="<?=base_url('') ?>assets/img/16.jpg" alt="img16"/></a>
+            <figcaption>
+              <h2 class="photostack-title">Lightness</h2>
+            </figcaption>
+          </figure>
         </div>
-        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-          <img src="assets/img/logo/logo.png" width="200%" class="img-fluid" alt="">
-        </div>
-      </div>
-
-      <!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="assets/img/slider/contohh.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="assets/img/slider/contohh.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="assets/img/slider/contohh.jpg" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-        </div> -->
-    </div>
-
-  </section><!-- End Hero -->
+      </section>
+    </section><!-- End Hero -->
+    <script src="<?=base_url('') ?>assets/js/classie.js"></script>
+    <script src="<?=base_url('') ?>assets/js/photostack.js"></script>
+    <script>
+      // [].slice.call( document.querySelectorAll( '.photostack' ) ).forEach( function( el ) { new Photostack( el ); } );
+      
+      new Photostack( document.getElementById( 'photostack-1' ), {
+        callback : function( item ) {
+          //console.log(item)
+        }
+      } );
+      new Photostack( document.getElementById( 'photostack-2' ), {
+        callback : function( item ) {
+          //console.log(item)
+        }
+      } );
+      new Photostack( document.getElementById( 'photostack-3' ), {
+        callback : function( item ) {
+          //console.log(item)
+        }
+      } );
+    </script>
+  </body>
+</html>
