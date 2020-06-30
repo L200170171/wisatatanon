495cafab7feaaf2028a3070690037072723d57d8
Giovanny Dani Saputra <47962353+L200170159@users.noreply.github.com>
2020-07-01 06:19:51 +0700

    swal notif

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,29 +20,13 @@ $('.tombol-hapus').on('click',function(e){
       })
 })
 
-$('.admin-hapus').on('click',function(e) {
-  e.preventDefault();
+// notif
+const flashData = $('.flash-data').data('flashdata');
 
-  // const linkdelete=$(this).attr('href');
-
-  Swal.fire({
-    title: 'Masukan Password',
-    icon: 'warning',
-    html:
-    '<form>'+
-    '<div class="form-group">'+
-      '<div>'+
-        '<input type="password" placeholder="Password" class="form-control" id="inputPassword">'+
-      '</div>'+
-    '</div>'+
-  '</form>',
-    showCloseButton: true,
-    showCancelButton: true,
-    focusConfirm: false,
-    confirmButtonText:
-      'Submit',
-    cancelButtonText:
-      'Cancel'
-  })
-})
-
+if (flashData){
+    Swal.fire ({
+        icon: 'success',
+        title:flashData
+    }); 
+        
+}
