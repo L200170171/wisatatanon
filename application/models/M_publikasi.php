d26f0cb7e05431c23216ffc971751bc5e25ec80a
unknown <oppet.copet@gmail.com>
2020-07-04 01:17:12 +0700

    backend highlight galeri testi

---
+++ application/models/M_publikasi.php
@@ -0,0 +1,39 @@
+<?php
+
+class M_publikasi extends CI_model{
+    public function cek($id){
+        $cek = $this->db->get_where('publikasi',['ID_testimoni'=>$id])->row_array();
+        if($cek){
+            return TRUE;
+        }
+        else{
+            return FALSE;
+        }
+    }
+
+    function sidebar(){
+        $query = $this->db->order_by('agenda_mulai', ' DESC');
+        $query = $this->db->get('tbl_agenda',3);    
+        return $query->result_array();
+    }
+
+    public function t_data(){
+        $this->db->select('*');
+        $this->db->from('publikasi');
+        $this->db->join('testimoni', 'testimoni.ID_testimoni = publikasi.ID_testimoni');
+        $query = $this->db->order_by('tanggal', ' DESC');
+        $this->db->limit(6);
+        $query = $this->db->get();
+        return $query->result_array();
+        }
+
+    public function insert($id){
+        $this->db->insert('publikasi',['ID_testimoni'=>$id]);
+    }
+
+    public function delete($id){
+        $this->db->where('ID_testimoni',$id);
+        $this->db->delete('publikasi');
+    }
+
+}
---
+++ application/models/M_testimoni.php
@@ -0,0 +1,23 @@
+<?php
+
+class M_testimoni extends CI_model{
+    public function t_testi(){
+        $query=$this->db->get("testimoni");
+        return $query->result_array();
+    }
+
+    public function insert(){
+        $data = [
+            "nama" => htmlspecialchars($this->input->post("nama-anda",true)),
+            "instansi" => htmlspecialchars($this->input->post("instansi",true)),
+            "testimoni" => htmlspecialchars($this->input->post("testimoni",true))
+        ];
+        $this->db->insert('testimoni',$data);
+    }
+
+    public function delete($id){
+        $this->db->where('ID_testimoni',$id);
+        $this->db->delete('publikasi');
+    }
+
+}
