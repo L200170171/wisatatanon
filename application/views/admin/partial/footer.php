1a3ad0e82c6d4fdfdeb7fd5bf50823ec93237376
Giovanny Dani Saputra <47962353+L200170159@users.noreply.github.com>
2020-07-20 14:34:33 +0700

    footer

--- application/views/admin/partial/footer.php
+++ application/views/admin/partial/footer.php
@@ -5,3 +5,10 @@
       <b>Version</b> 3.0.5
     </div>
   </footer>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
