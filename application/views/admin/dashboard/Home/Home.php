e8203a862a1e8cbf1154380f1447bd71d6b7ab66
Giovanny Dani Saputra <47962353+L200170159@users.noreply.github.com>
2020-07-25 14:38:08 +0700

    home dashboard admin

--- application/views/admin/dashboard/Home/Home.php
+++ application/views/admin/dashboard/Home/Home.php
@@ -1,5 +1,71 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+      <!-- Main content -->
+      <section class="content">
+        <div class="container">
+          <div class="card">
+            <div class="card-body">
+              <div class="container-fluid">
+                <div class="col-lg-12 col-9">
+                  <div class="container">
+                    <h1 class="display-4 text-center"><b>Selamat Datang!</b></h1>
+                  </div>
+                </div>
+                <!-- Small boxes (Stat box) -->
+                <div class="row pt-3 justify-content-center">
+                  <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                      <div class="inner">
+                        <h3>150</h3>
+        
+                        <p>Jumlah Artikel</p>
+                      </div>
+                      <div class="icon">
+                        <i class="fa fa-newspaper-o"></i>
+                      </div>
+                      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                      <div class="inner">
+                        <h3>150</h3>
+        
+                        <p>Jumlah Paket</p>
+                      </div>
+                      <div class="icon">
+                        <i class="fa fa-list-ul"></i>
+                      </div>
+                      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <!-- ./col -->
+                  <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                      <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+        
+                        <p>Pengunjung</p>
+                      </div>
+                      <div class="icon">
+                        <i class="fa fa-chart-bar"></i>
+                      </div>
+                      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <!-- ./col -->
+                </div>
+                <!-- /.row -->
+              </div><!-- /.container-fluid -->
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
